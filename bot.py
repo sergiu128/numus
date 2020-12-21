3ec56fc633184b5cd4c0241e34bee5ad223982b4
sergiu128 <sergiu128@protonmail.com>
2020-12-22 00:00:14 +0200

    can make private api requests

--- bot.py
+++ bot.py
@@ -1,8 +1,69 @@
+#!/usr/bin/python
+import sys
+import hmac
 import json
+import time
+import uuid
+import hashlib
 import logging
 import requests
+
+from urllib.parse import urlencode
 from telegram.ext import Updater, CommandHandler
 
+with open('./.bitstamp', 'r') as fin:
+    a = json.load(fin)
+    client_id = a['client_id']
+    main_key = a['main_key']
+    main_secret = a['main_secret']
+
+api_key = main_key
+API_SECRET = bytes(main_secret, 'utf-8')
+
+timestamp = str(int(round(time.time() * 1000)))
+nonce = str(uuid.uuid4())
+content_type = 'application/x-www-form-urlencoded'
+payload = {'offset': '1'}
+
+payload_string = urlencode(payload)
+message = \
+    'BITSTAMP ' + api_key + \
+    'POST' + \
+    'www.bitstamp.net' + \
+    '/api/v2/user_transactions/' + \
+    '' + \
+    content_type + \
+    nonce + \
+    timestamp + \
+    'v2' + \
+    payload_string
+message = message.encode('utf-8')
+signature = hmac.new(API_SECRET, msg=message, digestmod=hashlib.sha256).hexdigest()
+headers = {
+    'X-Auth': 'BITSTAMP ' + api_key,
+    'X-Auth-Signature': signature,
+    'X-Auth-Nonce': nonce,
+    'X-Auth-Timestamp': timestamp,
+    'X-Auth-Version': 'v2',
+    'Content-Type': content_type
+}
+r = requests.post(
+    'https://www.bitstamp.net/api/v2/user_transactions/',
+    headers=headers,
+    data=payload_string
+)
+
+if not r.status_code == 200:
+    raise Exception('Status code not 200')
+
+string_to_sign = (nonce + timestamp + r.headers.get('Content-Type')).encode('utf-8') + r.content
+signature_check = hmac.new(API_SECRET, msg=string_to_sign, digestmod=hashlib.sha256).hexdigest()
+if not r.headers.get('X-Server-Auth-Signature') == signature_check:
+    raise Exception('Signatures do not match')
+
+print(r.content)
+
+exit()
 
 def get_market(symbol):
     resp = requests.get('https://www.bitstamp.net/api/v2/ticker/{}/'.format(symbol))
