01e23a50546cf09685b03d54d41beae1e7ab0626
sergiu128 <sergiu128@protonmail.com>
2020-12-30 19:17:03 +0200

    implemented handler: /market

--- bot/bot.py
+++ bot/bot.py
@@ -1,33 +1,32 @@
+#!/usr/bin/python
 import logging
+import json
 
-from api import public
-from api import private
-
-import util
+import api.public
+import api.interface
 
 from telegram.ext import Updater, CommandHandler
 
 
-config = util.load_config()
+with open('../config/config.json', 'r') as fin:
+    config = json.loads(fin.read())
+
 token = config['telegram']['bot_token']
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 
-def get_btc_market(update, context):
-    text = api.get_market('btceur')
-    context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+def market(update, context):
+    response = api.interface.market('bitstamp', 'btceur')
+    text = '{:<12} @ {:>12}\nvwap: {:>17} \nlast-trade: {:>9} \nvolume: {:>13}'.format(
+        ''.join(['bid ', response['top-bid']]),
+        ''.join([response['top-ask'], ' ask']),
+        response['top-ask'],
+        response['vwap'],
+        response['last-trade'],
+        response['volume'])
 
-
-def get_eth_market(update, context):
-    text = api.get_market('etheur')
-    context.bot.send_message(chat_id=update.effective_chat.id, text=text)
-
-
-def get_open_orders(update, context):
-    text = api.get_open_orders('tati')
-    if text == '': text = 'No open orders.'
     context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
 
@@ -35,13 +34,12 @@ def run():
     updater = Updater(token=token)
     dispatcher = updater.dispatcher
 
-    get_btc_market_handler = CommandHandler('btc', get_btc_market)
-    get_eth_market_handler = CommandHandler('eth', get_eth_market)
-    get_open_orders_handler = CommandHandler('open', get_open_orders)
+    market_handler = CommandHandler('market', market)
 
-    dispatcher.add_handler(get_btc_market_handler)
-    dispatcher.add_handler(get_eth_market_handler)
-    dispatcher.add_handler(get_open_orders_handler)
+    dispatcher.add_handler(market_handler)
 
     updater.start_polling()
+
+if __name__ == '__main__':
+    run()
 
