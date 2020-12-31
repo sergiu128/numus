e63e893ae54de348effb082cf1a045843583821e
sergiu128 <sergiu128@protonmail.com>
2020-12-31 13:13:48 +0200

    implement private api on bitstamp

--- api/private.py
+++ api/private.py
@@ -7,123 +7,91 @@ import requests
 
 from urllib.parse import urlencode
 
-import util
 
-
-config = util.load_config()
-client_id = config['bitstamp']['client_id']
-
-
-def get_user_transactions(account):
+def user_transactions(account, limit=10):
     route = '/api/v2/user_transactions/'
+    payload = {
+        'limit': str(limit)
+    }
 
+    response = _query(account, route, payload)
+    return response
+
+
+def open_orders(account, currency_pair='all'):
+    route = '/api/v2/open_orders/{}/'.format(currency_pair)
+
+    response = _query(account, route)
+    return response
+
+
+def order_status(account, order_id):
+    route = '/api/v2/order_status/'
+    payload = {
+        'offset': 1,
+        'id': order_id
+    }
+
+    response = _query(account, route, payload)
+    return response
+
+
+with open('config/config.json', 'r') as fin:
+    config = json.loads(fin.read())
+
+
+def _query(account, route, payload={}):
+    print('here payload {}'.format(payload))
     key = config['bitstamp'][account]['key']
-    secret = config['bitstamp'][account]['secret']
-
-    api_key = key
-    API_SECRET = bytes(secret, 'utf-8')
-
-    timestamp = str(int(round(time.time() * 1000)))
+    secret = bytes(config['bitstamp'][account]['secret'], 'utf-8')
     nonce = str(uuid.uuid4())
-    content_type = 'application/x-www-form-urlencoded'
-    payload = {'offset': '1'}
+    timestamp = str(int(round(time.time() * 1000)))
 
-    payload_string = urlencode(payload)
-    message = \
-        'BITSTAMP ' + api_key + \
-        'POST' + \
-        'www.bitstamp.net' + \
-        route + \
-        '' + \
-        content_type + \
-        nonce + \
-        timestamp + \
-        'v2' + \
-        payload_string
-    message = message.encode('utf-8')
-    signature = hmac.new(API_SECRET, msg=message, digestmod=hashlib.sha256).hexdigest()
+    payload_string, content_type = '', ''
+    if payload != {}:
+        payload_string = urlencode(payload)
+        content_type = 'application/x-www-form-urlencoded'
+
+    message = 'BITSTAMP {}POSTwww.bitstamp.net{}{}{}{}v2{}'\
+        .format(key, route, content_type, nonce, timestamp, payload_string)\
+        .encode('utf-8')
+
+    signature = hmac.new(secret, msg=message, digestmod=hashlib.sha256).hexdigest()
+
     headers = {
-        'X-Auth': 'BITSTAMP ' + api_key,
+        'X-Auth': 'BITSTAMP {}'.format(key),
         'X-Auth-Signature': signature,
         'X-Auth-Nonce': nonce,
         'X-Auth-Timestamp': timestamp,
         'X-Auth-Version': 'v2',
-        'Content-Type': content_type
     }
-    r = requests.post(
+
+    print(content_type)
+    if content_type != '':
+        headers['Content-Type'] = content_type
+
+    response = requests.post(
         'https://www.bitstamp.net{}'.format(route),
         headers=headers,
         data=payload_string
     )
 
-    if not r.status_code == 200:
-        raise Exception('Status code not 200 on route:{} {}'.format(route, r.status_code))
+    if not response.status_code == 200:
+        raise Exception('Status code not 200 on route:{} {}'.format(route, response.status_code))
 
-    string_to_sign = (nonce + timestamp + r.headers.get('Content-Type')).encode('utf-8') + r.content
-    signature_check = hmac.new(API_SECRET, msg=string_to_sign, digestmod=hashlib.sha256).hexdigest()
-    if not r.headers.get('X-Server-Auth-Signature') == signature_check:
+    if response.reason != 'OK':
+        raise Exception('Invalid {} call; reason: {}'.format(route, response.reason))
+
+    content_type = response.headers.get('Content-Type')
+    if content_type is None:
+        raise Exception('Cannot get Content-Type response header.')
+
+    string_to_sign = (nonce + timestamp + content_type).encode('utf-8') + response.content
+    signature_check = hmac.new(secret, msg=string_to_sign, digestmod=hashlib.sha256).hexdigest()
+
+    if not response.headers.get('X-Server-Auth-Signature') == signature_check:
         raise Exception('Signatures do not match')
 
-    text = json.loads(r.content)
-    return text
+    content = json.loads(response.content)
 
-
-def get_open_orders(account):
-    route = '/api/v2/open_orders/btceur/'
-
-    key = config['bitstamp'][account]['key']
-    secret = config['bitstamp'][account]['secret']
-
-    api_key = key
-    API_SECRET = bytes(secret, 'utf-8')
-
-    timestamp = str(int(round(time.time() * 1000)))
-    nonce = str(uuid.uuid4())
-    content_type = 'application/x-www-form-urlencoded'
-    payload = {'offset': '1'}
-
-    payload_string = urlencode(payload)
-    message = \
-        'BITSTAMP ' + api_key + \
-        'POST' + \
-        'www.bitstamp.net' + \
-        route + \
-        '' + \
-        content_type + \
-        nonce + \
-        timestamp + \
-        'v2' + \
-        payload_string
-    message = message.encode('utf-8')
-    signature = hmac.new(API_SECRET, msg=message, digestmod=hashlib.sha256).hexdigest()
-    headers = {
-        'X-Auth': 'BITSTAMP ' + api_key,
-        'X-Auth-Signature': signature,
-        'X-Auth-Nonce': nonce,
-        'X-Auth-Timestamp': timestamp,
-        'X-Auth-Version': 'v2',
-        'Content-Type': content_type
-    }
-    r = requests.post(
-        'https://www.bitstamp.net{}'.format(route),
-        headers=headers,
-        data=payload_string
-    )
-
-    if not r.status_code == 200:
-        raise Exception('Status code not 200 on route:{} {}'.format(route, r.status_code))
-
-    string_to_sign = (nonce + timestamp + r.headers.get('Content-Type')).encode('utf-8') + r.content
-    signature_check = hmac.new(API_SECRET, msg=string_to_sign, digestmod=hashlib.sha256).hexdigest()
-
-    if not r.headers.get('X-Server-Auth-Signature') == signature_check:
-        raise Exception('Signatures do not match')
-
-    content = json.loads(r.content)
-
-    text = []
-    for order in content:
-        text.append('{} {}BTC at {} [{}]'.format('buy' if order['type'] == '0' else 'sell', order['amount'], order['price'], order['datetime']))
-
-    return '\n'.join(text)
-
+    return content
