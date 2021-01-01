50e5af4efe9caee5f79754ea6aedb41c483501f5
sergiu128 <sergiu128@protonmail.com>
2021-01-01 18:02:10 +0200

    show currency prompt on /trades command

--- bot/handlers/trades.py
+++ bot/handlers/trades.py
@@ -1,16 +1,49 @@
+from bot import keyboard
+from bot.handlers import help as help_handler
+
 from api import interface
-from telegram.ext import CommandHandler
+
+from telegram.ext import (
+    CommandHandler,
+    ConversationHandler,
+    CallbackQueryHandler
+)
 
 
-def trades(update, context):
-    trades = interface.trades(context.user_data['exchange'], 'btceur')
+REPLY = 0
+
+
+def select_currency_pair(update, context):
+    markup = keyboard.generate_currency_pair(['btceur', 'etheur'])
+    update.message.reply_text(text='currency pair:', reply_markup=markup)
+
+    return REPLY
+
+
+def reply(update, context):
+    query = update.callback_query
+    query.answer
+
+    currency_pair = query.data
+    trades = interface.trades(context.user_data['exchange'], currency_pair)
 
     reply = []
     for trade in trades:
         reply.append('{} {} @ {}'.format(trade['side'], trade['amount'], trade['price']))
 
-    update.message.reply_text('\n'.join(reply))
+    query.edit_message_text('\n'.join(reply))
+
+    return ConversationHandler.END
 
 
 def generate():
-    return CommandHandler('trades', trades)
+    handler = ConversationHandler(
+        entry_points=[CommandHandler('trades', select_currency_pair)],
+        states = {
+            REPLY: [CallbackQueryHandler(reply)]
+        },
+        fallbacks=[help_handler.generate()]
+    )
+
+    return handler
+
