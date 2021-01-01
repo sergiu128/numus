5d9462bfad46a204d7e300f4da9ec1dd688487d7
sergiu128 <sergiu128@protonmail.com>
2021-01-01 17:53:14 +0200

    make /set bot settings persistent over restarts

--- bot/bot.py
+++ bot/bot.py
@@ -9,7 +9,7 @@ from bot.handlers import (
     set as set_handler,
 )
 
-from telegram.ext import Updater
+from telegram.ext import Updater, PicklePersistence
 
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -20,10 +20,18 @@ class Bot:
     def __init__(self, token):
         self.token = token
 
-    def register_handlers(self):
-        self.updater = Updater(token=self.token)
+        persistence = PicklePersistence(
+            'config/bot.pickle',
+            store_user_data=True,
+            store_bot_data=True,
+            single_file=True,
+            on_flush=False
+        )
+
+        self.updater = Updater(token=self.token, persistence=persistence)
         self.dispatcher = self.updater.dispatcher
 
+    def register_handlers(self):
         self.dispatcher.add_handler(market_handler.generate())
         self.dispatcher.add_handler(help_handler.generate())
         self.dispatcher.add_handler(trades_handler.generate())
