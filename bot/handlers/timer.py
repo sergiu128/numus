328d498f0db1ce1aa9a01daec83e466e1da1efe4
sergiu128 <sergiu128@protonmail.com>
2021-01-02 16:36:40 +0200

    /timer: add option to set single or repeat timers

--- bot/handlers/timer.py
+++ bot/handlers/timer.py
@@ -1,13 +1,23 @@
 import datetime
 import pytz
-from telegram.ext import CommandHandler
+from telegram.ext import (
+    CommandHandler,
+    CallbackQueryHandler,
+    ConversationHandler
+)
 
 from bot.handlers import (
     market as market_handler,
     trades as trades_handler,
     open as open_handler,
     balance as balance_handler,
+    help as help_handler,
 )
+
+from bot import keyboard
+
+
+STATE_REPLY = 0
 
 
 def describe():
@@ -24,6 +34,81 @@ def _parse_count(count):
         raise Exception('Invalid time unit {}. Use one of: s, m, h.'.format(count[-1]))
 
     return int(count[:-1]), count[-1]
+
+
+def _state_get_type(update, context):
+    print(context.user_data)
+    if len(context.args) != 2:
+        update.message.reply_text(describe())
+        return
+
+    command = _parse_command(context.args[0])
+    due, unit = _parse_count(context.args[1])
+
+    if 'timer' not in context.user_data:
+        context.user_data['timer'] = {}
+
+    context.user_data['timer']['chat_id'] = update.message.chat_id
+    context.user_data['timer']['command'] = command
+    context.user_data['timer']['due'] = due
+    context.user_data['timer']['unit'] = unit
+
+    markup = keyboard.generate_currency_pair(['once', 'repeat'])
+    update.message.reply_text(text='timer type:', reply_markup=markup)
+
+    return STATE_REPLY
+
+
+def _state_set_timer(update, context):
+    query = update.callback_query
+    query.answer()
+
+    chat_id = context.user_data['timer']['chat_id']
+    command = context.user_data['timer']['command']
+    due = context.user_data['timer']['due']
+    unit = context.user_data['timer']['unit']
+    timer_type = query.data
+
+    if unit == 's':
+        delta = datetime.timedelta(seconds=due)
+    elif unit == 'm':
+        delta = datetime.timedelta(minutes=due)
+    else:
+        delta = datetime.timedelta(hours=due)
+
+    callback_context = {
+        'chat_id': chat_id,
+        'command': command,
+        'user_data': context.user_data,
+    }
+
+    timezone = pytz.timezone('Europe/Bucharest')
+    if timer_type == 'once':
+        when = timezone.localize(datetime.datetime.now() + delta)
+        context.job_queue.run_once(callback=_callback,
+                                   when=when,
+                                   context=callback_context,
+                                   name=str(chat_id))
+    elif timer_type == 'repeat':
+        trigger_time = timezone.localize(datetime.datetime.now())
+        context.job_queue.run_repeating(callback=_callback,
+                                        interval=delta,
+                                        first=trigger_time,
+                                        context=callback_context,
+                                        name=str(chat_id))
+    else:
+        raise Exception('Invalid timer type.')
+
+    query.edit_message_text(
+        'timer set: trigger /{} {} {}{}.'.format(
+            command,
+            'every' if timer_type == 'repeat' else 'after',
+            due,
+            unit
+        )
+    )
+
+    return ConversationHandler.END
 
 
 def _callback(context):
@@ -68,35 +153,15 @@ def _callback(context):
     context.bot.send_message(chat_id=chat_id, text=promise.result())
 
 
-def _timer(update, context):
-    if len(context.args) != 2:
-        update.message.reply_text(describe())
-        return
-
-    command = _parse_command(context.args[0])
-    due, unit = _parse_count(context.args[1])
-
-    if unit == 's':
-        delta = datetime.timedelta(seconds=due)
-    elif unit == 'm':
-        delta = datetime.timedelta(minutes=due)
-    else:
-        delta = datetime.timedelta(hours=due)
-
-    timezone = pytz.timezone('Europe/Bucharest')
-    when = timezone.localize(datetime.datetime.now() + delta)
-
-    callback_context = {
-        'chat_id': update.message.chat_id,
-        'command': command,
-        'user_data': context.user_data,
-    }
-
-    context.job_queue.run_once(_callback, when, context=callback_context, name=str(update.message.chat_id))
-
-    update.message.reply_text('timer set: trigger /{} after {}{}.'.format(command, due, unit))
-
 
 def generate():
-    return CommandHandler('timer', _timer)
+    handler = ConversationHandler(
+        entry_points=[CommandHandler('timer', _state_get_type, pass_chat_data=True)],
+        states={
+            STATE_REPLY: [CallbackQueryHandler(_state_set_timer, pass_chat_data=True)]
+        },
+        fallbacks=[help_handler.generate()]
+    )
+
+    return handler
 
